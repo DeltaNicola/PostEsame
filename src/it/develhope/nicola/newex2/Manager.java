957b0b325a3d0b96d893f5f1ebbebc3bf87812c3
nicolameloni <delta.meloni@gmail.com>
2022-05-08 05:04:44 +0200

    inseriti commenti Javadoc in Manager.java

--- src/it/develhope/nicola/newex2/Manager.java
+++ src/it/develhope/nicola/newex2/Manager.java
@@ -1,6 +1,17 @@
 package it.develhope.nicola.newex2;
 
+/**
+ * record per gestire il manager di controllo
+ * @author nicolameloni
+ * @param name nome del manager
+ * @param surname cognome del manager
+ */
 public record Manager(String name, String surname) {
+
+    /**
+     * metodo per restituire la stringa manager
+     * @return nome e cognome del manager come stringa
+     */
     @Override
     public String toString() {
         return "Manager{" +
