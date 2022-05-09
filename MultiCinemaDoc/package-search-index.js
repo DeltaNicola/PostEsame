50ea930ac7e28adbee0ce1e5a1eeaef59b295a21
nicolameloni <delta.meloni@gmail.com>
2022-05-09 18:36:47 +0200

    Aggiornato JavaDoc MultiCinemaDoc

--- MultiCinemaDoc/package-search-index.js
+++ MultiCinemaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.develhope.nicola.newex2"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.develhope.nicola.MultiCinema"}];updateSearchResults();
